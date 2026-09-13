e32b627356598bcc75e9e1ca63fb642a7f5c788b
hatim85 <devconnect.hatim@gmail.com>
2026-09-13 05:35:16 +0530

    Updated base url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://khm-erp.duckdns.org/api',
   withCredentials: true, // Required for HttpOnly cookies
   headers: {
     'Content-Type': 'application/json',
